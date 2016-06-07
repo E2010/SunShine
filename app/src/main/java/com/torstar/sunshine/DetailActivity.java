24a62c88b4cb886f4ade84160e0b118495c6e8d1
Eileen Zhang <eileen.zhang@metroland.com>
2016-06-07 14:07:28 -0400

    5.39 finish the redlines

--- app/src/main/java/com/torstar/sunshine/DetailActivity.java
+++ app/src/main/java/com/torstar/sunshine/DetailActivity.java
@@ -1,11 +1,8 @@
 package com.torstar.sunshine;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -28,15 +25,6 @@ public class DetailActivity extends AppCompatActivity {
                     .replace(R.id.weather_detail_container, detailActivityFragment)
                     .commit();
         }
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
--- app/src/main/java/com/torstar/sunshine/MainActivity.java
+++ app/src/main/java/com/torstar/sunshine/MainActivity.java
@@ -5,8 +5,6 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -61,15 +59,6 @@ public class MainActivity extends AppCompatActivity implements MainActivityFragm
 //                    .add(R.id.fragment,new MainActivityFragment(), FORECASTFRAGMENT_TAG)
 //                    .commit();
 //        }
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
